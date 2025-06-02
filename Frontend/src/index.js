07c4786a837427bd4caa58cecf1d64523533808d
edu gonzalez <320ego@gmail.com>
2025-06-02 20:38:09 +0200

    Create index.js

---
+++ Frontend/src/index.js
@@ -0,0 +1,17 @@
+import { EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { DOMParser } from "prosemirror-model";
+import { exampleSetup } from "prosemirror-example-setup";
+import { mySchema } from "./schema";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const editorContainer = document.getElementById("editor");
+  if (!editorContainer) return;
+
+  new EditorView(editorContainer, {
+    state: EditorState.create({
+      doc: DOMParser.fromSchema(mySchema).parse(editorContainer),
+      plugins: exampleSetup({ schema: mySchema })
+    })
+  });
+});
