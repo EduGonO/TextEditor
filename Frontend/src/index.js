1a49307e784d633660aaad2cab5039eae44e970e
edu gonzalez <320ego@gmail.com>
2025-06-02 22:58:39 +0000

    editor

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,10 +1,16 @@
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { DOMParser } from "prosemirror-model";
-import { toggleMark } from "prosemirror-commands";
-import { wrapInList } from "prosemirror-schema-list";
 import { exampleSetup } from "prosemirror-example-setup";
+import { toggleMark } from "prosemirror-commands";
+import { liftListItem, wrapInList } from "prosemirror-schema-list";
 import { mySchema } from "./schema";
+import { setBlockType, wrapIn } from "prosemirror-commands";
+import { undo, redo } from "prosemirror-history";
+import { keymap } from "prosemirror-keymap";
+import { baseKeymap } from "prosemirror-commands";
+import { history } from "prosemirror-history";
+import { sinkListItem } from "prosemirror-schema-list";
 
 window.addEventListener("DOMContentLoaded", () => {
   const editorContainer = document.getElementById("editor");
@@ -13,23 +19,95 @@ window.addEventListener("DOMContentLoaded", () => {
   const editor = new EditorView(editorContainer, {
     state: EditorState.create({
       doc: DOMParser.fromSchema(mySchema).parse(editorContainer),
-      plugins: exampleSetup({ schema: mySchema })
+      plugins: [
+        exampleSetup({ schema: mySchema })
+      ]
     })
   });
 
   window.editor = editor;
 
   window.toggleMark = function(mark) {
-    const markType = mySchema.marks[mark];
-    if (!markType) return;
-    const cmd = toggleMark(markType);
-    if (cmd) cmd(editor.state, editor.dispatch);
+    const type = mySchema.marks[mark];
+    if (!type) return;
+    const cmd = toggleMark(type);
+    cmd(editor.state, editor.dispatch);
   };
 
   window.wrapList = function(nodeType) {
+    const { state, dispatch } = editor;
     const listType = mySchema.nodes[nodeType];
     if (!listType) return;
-    const cmd = wrapInList(listType);
-    if (cmd) cmd(editor.state, editor.dispatch);
+
+    const isInList = state.selection.$from.path.some(n => n.type === listType);
+    if (isInList) {
+      // Remove list by lifting items out
+      const itemType = mySchema.nodes.list_item;
+      if (itemType) {
+        liftListItem(itemType)(state, dispatch);
+      }
+    } else {
+      wrapInList(listType)(state, dispatch);
+    }
   };
+
+  window.wrapInBlock = function(nodeType) {
+    const type = mySchema.nodes[nodeType];
+    if (!type) return;
+    const cmd = wrapIn(type);
+    cmd(editor.state, editor.dispatch);
+  };
+
+  window.setBlock = function(nodeType, attrs = {}) {
+    const type = mySchema.nodes[nodeType];
+    if (!type) return;
+    const cmd = setBlockType(type, attrs);
+    cmd(editor.state, editor.dispatch);
+  };
+
+  window.undo = function() {
+    undo(editor.state, editor.dispatch);
+  };
+
+  window.redo = function() {
+    redo(editor.state, editor.dispatch);
+  };
+
+  window.insertLink = function(href) {
+    const { schema, state, dispatch } = editor;
+    const markType = schema.marks.link;
+    if (!markType) return;
+
+    const { from, to } = state.selection;
+    dispatch(state.tr.addMark(from, to, markType.create({ href })));
+  };
+  window.indent = function() {
+    const { state, dispatch } = editor;
+    const itemType = mySchema.nodes.list_item;
+    if (itemType) sinkListItem(itemType)(state, dispatch);
+  };
+
+  window.toggleTodo = function(input) {
+    const pos = editor.posAtDOM(input.closest("li"), 0);
+    const node = editor.state.doc.nodeAt(pos);
+    const tr = editor.state.tr.setNodeMarkup(pos, null, {
+      ...node.attrs,
+      checked: !node.attrs.checked
+    });
+    editor.dispatch(tr);
+  };
+
+  window.insertTodo = function() {
+    const { state, dispatch } = editor;
+    const type = mySchema.nodes.todo_item;
+    if (!type) return;
+    dispatch(state.tr.replaceSelectionWith(type.createAndFill()));
+  };
+
+  window.outdent = function() {
+    const { state, dispatch } = editor;
+    const itemType = mySchema.nodes.list_item;
+    if (itemType) liftListItem(itemType)(state, dispatch);
+  };
+  window.editorReady = true;
 });
--- Frontend/src/schema.js
+++ Frontend/src/schema.js
@@ -3,6 +3,34 @@ import { schema as basicSchema } from "prosemirror-schema-basic";
 import { addListNodes } from "prosemirror-schema-list";
 
 export const mySchema = new Schema({
-  nodes: addListNodes(basicSchema.spec.nodes, "paragraph block*", "block"),
+  nodes: addListNodes(
+    basicSchema.spec.nodes.append({
+      todo_item: todoItem
+    }),
+    "paragraph block*",
+    "block"
+  ),
   marks: basicSchema.spec.marks
 });
+
+export const todoItem = {
+  attrs: { checked: { default: false } },
+  content: "paragraph",
+  toDOM: node => [
+    "li",
+    { "data-type": "todo-item" },
+    ["input", {
+      type: "checkbox",
+      checked: node.attrs.checked ? "checked" : null,
+      onclick: "window.toggleTodo && window.toggleTodo(this)"
+    }],
+    ["div", 0]
+  ],
+  parseDOM: [{
+    tag: "li[data-type=todo-item]",
+    getAttrs(dom) {
+      const input = dom.querySelector("input[type=checkbox]");
+      return { checked: input?.checked };
+    }
+  }]
+};
